aaa8d2bb4220aca39db333e10c478681599dc16f
alexppapp <129535732+alexppapp@users.noreply.github.com>
2023-06-04 21:55:13 +0300

    Update indexes.sql

--- Mysql/indexes.sql
+++ Mysql/indexes.sql
@@ -2,3 +2,23 @@ use SCHOOL;
 
 create index idx_name1
 on Main_Handler(Username);
+
+create index idx_name2
+on Users(Username);
+
+create index idx_age
+on Users(Age);
+
+create index idx_name3
+on Handlers(Username);
+
+create index idx_isbn
+on Books(ISBN);
+
+create index idx_theme
+on Books(Theme);
+
+create index idx_author
+on Books(Author);
+
+
